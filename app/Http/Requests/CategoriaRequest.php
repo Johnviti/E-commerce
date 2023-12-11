87cb6ba13c00dfe1c36bda09f1f348e3c62f8424
Johnviti <jvsa5@aluno.ifal.edu.br>
2023-12-11 00:41:11 -0300

    Início do Crud de categoria de produtos

--- app/Http/Requests/CategoriaRequest.php
+++ app/Http/Requests/CategoriaRequest.php
@@ -4,14 +4,14 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ProdutoRequest extends FormRequest
+class CategoriaRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return True;
+        return true;
     }
 
     /**
@@ -22,23 +22,17 @@ class ProdutoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'codigo' => 'required',
             'descricao' => 'required',
-            'unidade' => 'required',
-            'peso' => 'required',
-            'preco' => 'required',
-            'id_categoria_produto' => 'required'
+            'observacao' => 'required'
         ];
     }
 
     public function messages(): array
     {
         return [
-            'codigo.required' => 'O campo código é obrigatório',
             'descricao.required' => 'O campo descrição é obrigatório',
-            'unidade.required' => 'O campo unidade é obrigatório',
-            'peso.required' => 'O campo peso é obrigatório',
-            'preco.required' => 'O campo preço é obrigatório'
+            'observacao.required' => 'O campo observação é obrigatório'
         ];
     }
+
 }
